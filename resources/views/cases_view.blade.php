002a800d14e6c7a2aa6416bbe6c569c5ef2d43c2
Aryel Tupinamba <aryel.tupinamba@lqdi.net>
2018-07-30 18:36:31 -0300

    added more screens to cases view

--- resources/views/cases_view.blade.php
+++ resources/views/cases_view.blade.php
@@ -168,6 +168,205 @@
 
 				</div>
 
+				<div v-if="openTab === 'tags'">
+					<div class="row">
+						<div class="col-md-12">
+							<label class="detail__label">ETIQUETAS</label>
+
+							<div class="row">
+
+								<div class="col-md-12 text-center">
+									<button type="button" class="btn btn-lg btn-primary"><i class="fa fa-plus"></i> Adicionar etiqueta</button>
+									<hr />
+								</div>
+
+								@foreach($labels->shuffle()->take(rand(4,7)) as $label)
+									<div class="col-md-3">
+										<div class="card">
+											<div class="card-header">
+												<strong><i class="fa fa-tag"></i> {{$label}}</strong>
+												<button type="button" v-b-tooltip title="Remover etiqueta" class="btn btn-sm btn-outline-danger pull-right"><i class="fa fa-trash"></i></button>
+											</div>
+											<div class="card-body">
+												<div><i v-b-tooltip title="Data de atribuição" class="fa fa-calendar"></i> {{$faker->date('d/m/Y')}}</div>
+												<div><i v-b-tooltip title="Operador responsável" class="fa fa-user"></i> {{$faker->userName}}</div>
+												<div><i v-b-tooltip title="Prazo para resolução" class="fa fa-clock"></i> {{$faker->date('d/m/Y')}} (em {{rand(2, 60)}} dias)</div>
+											</div>
+										</div>
+										<br />
+									</div>
+								@endforeach
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div v-if="openTab === 'home'">
+					<div class="row">
+						<div class="col-md-8">
+							<label class="detail__label">DOMICÍLIO</label>
+							<h3>{{$faker->address}}</h3>
+						</div>
+						<div class="col-md-4">
+							<label class="detail__label">ETIQUETAS</label>
+							<div>
+								@foreach($labels->shuffle()->take(rand(3,5)) as $label)
+									<span class="badge badge-secondary">{{$label}}</span>
+								@endforeach
+							</div>
+						</div>
+					</div>
+
+					<hr />
+
+					<div class="row">
+						<div class="col-md-3">
+							<label class="detail__label">FORMULÁRIOS</label>
+
+							<div class="list-group mt-3">
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Preenchido" class="fa fa-circle text-success"></i> Formulário de Alerta / PNUD</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> Cadastro de Domicílio</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action active"><i v-b-tooltip title="Em preenchimento" class="fa fa-circle text-warning"></i> Falta de Saneamento Básico</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> Falta de Energia Elétrica</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> Falta de Transporte Público</a>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<label class="detail__label">Formulário: <strong>Falta de Saneamento Básico</strong></label>
+
+							<form class="card mt-3">
+								<div class="card-body">
+									<div class="form-group">
+										<label for="fld-test">Casa possui sistema de encanamento?</label>
+										<div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="hasPlumbing" id="plumbing-yes" value="yes">
+												<label class="form-radio-label" for="plumbing-yes">Sim</label>
+											</div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="hasPlumbing" id="plumbing-no" value="no">
+												<label class="form-radio-label" for="plumbing-no">Não</label>
+											</div>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Qual o endereço mais próximo que possui acesso à saneamento básico?</label>
+										<input class="form-control" type="text" />
+										<small>Preencha o endereço completo, com CEP.</small>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Houve tentativa de contato com a empresa de saneamento urbano?</label>
+										<div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="hasContacted" id="contacted-yes" value="yes">
+												<label class="form-radio-label" for="contacted-yes">Sim</label>
+											</div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="hasContacted" id="contacted-no" value="no">
+												<label class="form-radio-label" for="contacted-no">Não</label>
+											</div>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Quantas pessoas moram na mesma habitação?</label>
+										<input class="form-control" type="number" />
+									</div>
+								</div>
+								<div class="card-footer">
+									<a href="#" class="btn btn-outline-primary"><i class="fa fa-save"></i> Salvar e continuar editando</a>
+									<a href="#" class="btn btn-primary pull-right"><i class="fa fa-upload"></i> Submeter formulário</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+
+				<div v-if="openTab === 'family'">
+					<div class="row">
+						<div class="col-md-8">
+							<label class="detail__label">FAMÍLIA</label>
+							<h3>Responsável: João Silva Andrade</h3>
+						</div>
+						<div class="col-md-4">
+							<label class="detail__label">ETIQUETAS</label>
+							<div>
+								@foreach($labels->shuffle()->take(rand(3,5)) as $label)
+									<span class="badge badge-secondary">{{$label}}</span>
+								@endforeach
+							</div>
+						</div>
+					</div>
+
+					<hr />
+
+					<div class="row">
+						<div class="col-md-3">
+							<label class="detail__label">FORMULÁRIOS</label>
+
+							<div class="list-group mt-3">
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Preenchido" class="fa fa-circle text-success"></i> Formulário de Alerta / PNUD</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> Cadastro de Família</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action active"><i v-b-tooltip title="Em preenchimento" class="fa fa-circle text-warning"></i> Enquadramento no Bolsa Família</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> União civil sem registro</a>
+								<a href="#" class="list-group-item small p-2 list-group-item-action"><i v-b-tooltip title="Não preenchido" class="fa fa-circle text-secondary"></i> Avaliação de assistência social</a>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<label class="detail__label">Formulário: <strong>Enquadramento no Bolsa Família</strong></label>
+
+							<form class="card mt-3">
+								<div class="card-body">
+									<div class="form-group">
+										<label for="fld-test">Faixa de renda familiar</label>
+										<div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="familyIncome" id="income-upTo500" value="upTo500">
+												<label class="form-radio-label" for="income-upTo500">até R$ 500 / mês</label>
+											</div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="familyIncome" id="income-500to1000" value="500to1000">
+												<label class="form-radio-label" for="income-500to1000">R$ 500 a R$ 1.000 / mês</label>
+											</div>
+											<div class="form-radio form-radio-inline">
+												<input class="form-radio-input" type="radio" name="familyIncome" id="income-over1000" value="over1000">
+												<label class="form-radio-label" for="income-over1000">acima de R$ 1.000 / mês</label>
+											</div>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Genitora</label>
+										<input class="form-control" type="text" />
+										<small>Preencha o nome completo da mãe ou filiação primária, se houver</small>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Genitor</label>
+										<input class="form-control" type="text" />
+										<small>Preencha o nome completo do pai ou filiação secundária, se houver</small>
+									</div>
+
+									<div class="form-group">
+										<label for="fld-test">Quantas crianças em idade escolar estão presentes na família?</label>
+										<input class="form-control" type="number" />
+										<div>
+											<small>Indique somente as crianças do núcleo familiar selecionado, que residem na mesma residência.</small>
+										</div>
+									</div>
+								</div>
+								<div class="card-footer">
+									<a href="#" class="btn btn-outline-primary"><i class="fa fa-save"></i> Salvar e continuar editando</a>
+									<a href="#" class="btn btn-primary pull-right"><i class="fa fa-upload"></i> Submeter formulário</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+
 				<div v-if="openTab === 'member1' || openTab === 'member2' || openTab === 'member3'">
 					<div class="row">
 						<div class="col-md-8">
